2bcb778ae18c50abda2d68724f8129f1a211ee94
Alfonso Mondragon <hotdogponcho@ciencias.unam.mx>
2022-03-01 15:59:13 -0600

    Implementacion de algunas funciones

--- Practica01.LC/Practica1.hs
+++ Practica01.LC/Practica1.hs
@@ -1,7 +1,20 @@
+{--anagrama :: [a] -> [a] -> Bool
+anagrama x y = False
+
+segmento :: Int -> Int -> [a] 
+segmento x y = []
+
+prodReps :: [a] -> Int
+prodReps [x] = 1
+
+
+elimina :: [a] -> Int -> [a]
+elimina [x] i = []--}
+
 {--
 ---	Equipo Omeguita
 ---	Alvarado Camacho Andrea		318064343
----	Mondrsgon Segoviano Alfonso	115000957
+---	Mondragón Segoviano Alfonso	115000957
 --}
 
 module Practica1 where
@@ -14,8 +27,18 @@ anagrama s1 s2 = error "Sin implementar."
 
 --2. segmento. Función que devuelve la parte de la lista 
 --             comprendida por los índices.
-segmento :: Int -> Int -> [Int]
-segmento n m = error "Sin implementar."
+segmento :: Int -> Int -> [Int] -> [Int] 
+segmento n _ _
+    | n < 0 = [] --Indice inicio menor a 0
+segmento _ m (x:xs)
+    | m > length (x:xs) = [-1] --Indice de fin mayor a longitud del arreglo   
+segmento _ _ [] = [] --Cuando se ingresa una lista vacía 
+segmento n m (x:xs)
+    | n == 0 = if m <= -1 -- Para indicar el fin de la iteración
+                    then []
+                    else [x] ++ segmento n (m-1) (xs)
+    | otherwise = segmento (n-1) (m-1) (xs) -- Cuando aún no entramos dentro del rango 
+
 
 
 --3. prodReps. Función que devuelve el producto del número con más 
@@ -26,12 +49,26 @@ prodReps l = error "Sin implementar."
 
 --4. esEspejo. Función que determina si una fecha es espejo.
 esEspejo :: String -> Bool
-esEspejo s = error "Sin implementar."
+esEspejo [] = False
+esEspejo [x] = True
+esEspejo (x:xs) = if reversa (x:xs) == (x:xs)
+                    then True
+	                else False
+
+--Función auxiliar para esEspejo.
+reversa :: String -> String
+reversa [] = []
+reversa (x:xs) = (reversa xs) ++ [x]
 
 
 --5. elimina. Función que elimina de la lista el número del índice.
 elimina :: [Int] -> Int -> [Int]
-elimina l n = error "Sin implementar."
+elimina [] _ = []
+elimina _ n 
+    | n < 0 = []
+elimina (x:xs) n 
+    | n == 0 = xs
+    | otherwise = [x] ++ elimina (xs) (n-1)
 
 
 --6. Binario. Tipo de dato para representación de binario.
