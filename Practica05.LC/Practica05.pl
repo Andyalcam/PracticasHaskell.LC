bb36afc7a82be2e86619a8689175a24be7166bd2
Poncho Mondragón <hotdogponcho@ciencias.unam.mx>
2022-05-14 22:10:10 -0500

    Inicio de Practica

--- Practica05.LC/Practica05.pl
+++ Practica05.LC/Practica05.pl
@@ -1,10 +1,22 @@
 pertenece(E, [X|XS]) :- E = X; pertenece(E, XS).
 
-longitud([],0).
-longitud([_|XS],N) :- longitud(XS, M), N is M+1.
+/*longitud([],0).
+longitud([_|XS],N) :- longitud(XS, M), N is M+1.*/
 
-/* indice(_,[],-1).
-	indice(E,[X],0) :- E = X.
-	indice(E,[X|XS],I) :- indice(E,[XS],N) , I is N+1. 
-*/
+/*indice(_,[],-1).
+indice(E,[X],1) :- E = X.
+% indice(E,[X],Y) :- E = X.
+indice(E,[X|XS],I) :- E = X; indice(E, [XS], N), I is N+1.*/
 
+
+destino(los_mochis, el_fuerte).
+destino(el_fuerte, bahuichivo).
+destino(bahuichivo, divisadero).
+destino(divisadero, creel).
+
+destino(X,Y,L) :- destino(X,Y), L = [X|[Y]].
+
+viaje(X,Y,L) :- destino(X,Z),destino(Z,Y), L = [1].
+
+fst([X|_],X).  
+snd([_|XS],XS). 
