ec59516534e8e76772981d25c25ba1a643b5b8c9
Andyalcam <andreaalcam@ciencias.unam.mx>
2022-05-19 01:08:17 -0500

    Final de practica

--- Practica05.LC/Practica05.pl
+++ Practica05.LC/Practica05.pl
@@ -1,22 +1,67 @@
+pertenece(E,[X]) :- E = X.
 pertenece(E, [X|XS]) :- E = X; pertenece(E, XS).
 
-/*longitud([],0).
-longitud([_|XS],N) :- longitud(XS, M), N is M+1.*/
-
-/*indice(_,[],-1).
-indice(E,[X],1) :- E = X.
-% indice(E,[X],Y) :- E = X.
-indice(E,[X|XS],I) :- E = X; indice(E, [XS], N), I is N+1.*/
-
+indice(X,[X|_],0).
+indice(E,[_|XS],I) :- indice(E, XS, N), I is N+1.
 
 destino(los_mochis, el_fuerte).
 destino(el_fuerte, bahuichivo).
 destino(bahuichivo, divisadero).
 destino(divisadero, creel).
+regreso(X,Y) :- destino(Y,X).
+viaje(W,Z) :- destino(W,Z).
+viaje(X,Z) :- destino(X,Y), viaje(Y,Z).
+viajeR(W,Z) :- regreso(W,Z).
+viajeR(X,Z) :- regreso(X,Y), viajeR(Y,Z).
 
-destino(X,Y,L) :- destino(X,Y), L = [X|[Y]].
+estacionesI(X,Z,[X,Z]) :- destino(X,Z).
+estacionesI(X,Z,[X|C]) :- destino(X,Y), estacionesI(Y,Z,C).
 
-viaje(X,Y,L) :- destino(X,Z),destino(Z,Y), L = [1].
+estacionesR(X,Z,[X,Z]) :- regreso(X,Z).
+estacionesR(X,Z,[X|C]) :- regreso(X,Y), estacionesR(Y,Z,C).
 
-fst([X|_],X).  
-snd([_|XS],XS). 
+modCero(N,X) :- 0 is N mod X.
+ 
+numDivAux(_,1,1).
+numDivAux(N,X,ND) :- X>1, modCero(N,X), NX is X-1, numDivAux(N,NX,SD), ND is 1+SD.
+numDivAux(N,X,ND) :- X>1, not(modCero(N,X)), NX is X-1, numDivAux(N,NX,ND).
+
+numDiv(N,ND) :- numDivAux(N,N,ND).
+ 
+primo(N) :- numDiv(N,2).
+
+elimina_dups([],[]).
+elimina_dups([X],[X]).
+elimina_dups([X,X|XS],L) :- elimina_dups([X|XS],L).
+elimina_dups([X,Y|YS],[X|ZS]) :- X \= Y, elimina_dups([Y|YS],ZS).
+
+gustar(alexia,leer).
+gustar(david,leer).
+clima(nuboso).
+clima(lluvioso).
+odio(alexia,clima_lluvioso).
+queLee(alexia,alicia_en_el_Pais_de_las_Maravillas,lewis_Carrol).
+lee(david,clima_lluvioso).
+lee(david,clima_nuboso).
+
+
+esposo(autor,viuda).
+esposo(padre,hija1).
+hija(hija1,viuda).
+hija(hija1,autor).
+hija(hija2,hija1).
+hija(hija2,padre).
+hijo(autor,padre).
+hijo(padre,autor).
+hijo(hijo3,viuda).
+hijo(hijo3,autor).
+suegro(X,Y) :- (hija(A,X);hijo(A,X)), esposo(A,Y).
+madrastra(X,Y) :- esposo(X,A), (hija(Y,A);hijo(Y,A)).
+yerno(X,Y) :- esposo(X,A), hija(A,Y).
+hermanos(X,Y) :- (hija(X,A);hijo(X,A)), (hija(Y,A);hijo(Y,A)).
+nieta(X,Y) :- (hija(X,A); hijo(X,A)), (hija(A,Y); hijo(A,Y)).
+abuelo(X,Y) :- (hija(Y,A); hijo(Y,A)), (hija(A,X); hijo(A,X)).
+cuñado(X,Y) :- esposo(X,A), hermanos(A,Y).
+tio(X,Y) :- hermanos(X,A), (hija(Y,A);hijo(Y,A)).
+nuera(X,Y) :- esposo(A,X), hijo(A,Y).
+bisnieto(X,Y) :- (hija(X,A); hijo(X,A)), (hija(A,B); hijo(A,B)), (hija(B,Y); hijo(B,Y)).
